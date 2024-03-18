7a6edeac6a95a27ddcf0f741980bbf1d85e65ac6
ElrohirGT <elrohirgt@gmail.com>
2024-03-18 16:00:16 -0600

    feat: Increased the JSON body limit

--- Lab6/backend/index.js
+++ Lab6/backend/index.js
@@ -1,35 +1,36 @@
-import cors from 'cors'
-import express from 'express'
-import { logger, logMiddleware } from './routes/lib.js'
-import notFoundRouter from './routes/notfound.js'
-import postsRouter from './routes/posts.js'
-import postIdRouter from './routes/posts[postid].js'
-import unsupportedMethodsRouter from './routes/unsupported.js'
-import 'dotenv/config.js'
+import cors from "cors";
+import express from "express";
+import bodyParser from "body-parser";
+import { logger, logMiddleware } from "./routes/lib.js";
+import notFoundRouter from "./routes/notfound.js";
+import postsRouter from "./routes/posts.js";
+import postIdRouter from "./routes/posts[postid].js";
+import unsupportedMethodsRouter from "./routes/unsupported.js";
+import "dotenv/config.js";
 
-const app = express()
-const host = 'localhost'
-const port = 3000
-const docsHandler = express.static('static/docs/')
+const app = express();
+const host = "localhost";
+const port = 3000;
+const docsHandler = express.static("static/docs/");
 const corsConfig = {
   origin: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
 
-app.use(logMiddleware) // Log requests and responses
-app.use(express.json()) // Parse JSON in the body
-app.use(docsHandler)
-app.use(cors(corsConfig))
+app.use(logMiddleware); // Log requests and responses
+app.use(docsHandler);
+app.use(cors(corsConfig));
+app.use(bodyParser.json({ limit: 50 * 1024 * 1024 })); // Add a limit to 50MB
 
-app.get('/', (_req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (_req, res) => {
+  res.send("Hello World!");
+});
 
-app.use(postsRouter)
-app.use(postIdRouter)
-app.use(unsupportedMethodsRouter)
-app.use(notFoundRouter)
+app.use(postsRouter);
+app.use(postIdRouter);
+app.use(unsupportedMethodsRouter);
+app.use(notFoundRouter);
 
 app.listen(port, host, () => {
-  logger.info(`Example app listening on ${host}:${port}`)
-})
+  logger.info(`Example app listening on ${host}:${port}`);
+});
