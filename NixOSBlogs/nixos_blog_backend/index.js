86740756c0a2f637576d864ac55e50d64d7ae4ea
ElrohirGT <elrohirgt@gmail.com>
2024-04-26 00:33:52 -0600

    fix: host and port

--- NixOSBlogs/nixos_blog_backend/index.js
+++ NixOSBlogs/nixos_blog_backend/index.js
@@ -1,12 +1,13 @@
 const { log, logger } = require("./src/utils/log.js");
 const express = require("express");
 const app = express();
+const host = "0.0.0.0";
 const port = 3000;
 
 app.use(logger);
 
 app.use(express.static("static"));
 
-app.listen(port, () => {
+app.listen(`${host}:${port}`, () => {
   log.info(`Example app listening on port ${port}`);
 });
