5ce3d0f120697f0ddbcdc19757c4490d533d12d5
ElrohirGT <elrohirgt@gmail.com>
2024-04-26 10:51:48 -0600

    feat: Added command to build web server app

--- NixOSBlogs/flake.nix
+++ NixOSBlogs/flake.nix
@@ -32,9 +32,33 @@
     forEachSystem = nixpkgs.lib.genAttrs (import systems);
   in {
     packages = forEachSystem (
-      system: {
+      system: let
+        pkgs = import nixpkgs {inherit system overlays;};
+      in {
         # For setting up devenv
         devenv-up = self.devShells.${system}.default.config.procfileScript;
+
+        build-web-server = pkgs.writeShellApplication {
+          name = "NixOS Blogs WebServer build";
+          runtimeInputs = with pkgs; [
+            ansi
+            zip
+          ];
+
+          text = ''
+            echo -e "$(ansi yellow)Building frontend...$(ansi reset)"
+            nix develop --impure --command bash -c "cd ./nixos_blog_frontend/ && yarn build"
+
+            echo -e "$(ansi yellow)Deleting static dir in backend if exists...$(ansi reset)"
+            rm -r ./nixos_blog_backend/static/ || rm -r ./nixos_blog_backend/static || true
+
+            echo -e "$(ansi yellow)Copying frontend build to backend...$(ansi reset)"
+            cp -r ./nixos_blog_frontend/dist ./nixos_blog_backend/static
+
+            echo -e "$(ansi yellow)Installing backend dependencies...$(ansi reset)"
+            nix develop --impure --command bash -c "cd ./nixos_blog_backend/ && yarn"
+          '';
+        };
       }
     );
 
