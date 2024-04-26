ffacb2a624f8cae3f0deb2ebbffcbb8494f41e97
ElrohirGT <elrohirgt@gmail.com>
2024-04-25 21:24:15 -0600

    feat: Added nix file for NixOS blog project

---
+++ NixOSBlogs/flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.11";
+    systems.url = "github:nix-systems/default";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    systems,
+    rust-overlay,
+    devenv,
+    ...
+  } @ inputs: let
+    overlays = [(import rust-overlay)];
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages = forEachSystem (
+      system: {
+        # For setting up devenv
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+      }
+    );
+
+    devShells = forEachSystem (system: let
+      pkgs = import nixpkgs {inherit system overlays;};
+    in {
+      default = devenv.lib.mkShell {
+        inherit pkgs inputs;
+        modules = [
+          {
+            packages = with pkgs; [
+              dprint
+              oxlint
+            ];
+
+            languages.javascript = {
+              enable = true;
+              yarn = {
+                enable = true;
+                install.enable = true;
+              };
+            };
+          }
+        ];
+      };
+    });
+  };
+}
