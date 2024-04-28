8e8d45d0d6922966a825144dd24ce51e38dd744c
ElrohirGT <elrohirgt@gmail.com>
2024-04-27 21:27:40 -0600

    fix: PostgreSQL in devenv

--- NixOSBlogs/flake.nix
+++ NixOSBlogs/flake.nix
@@ -1,13 +1,11 @@
 {
-  description = "A very basic flake";
+  description = "¡NixOS Blogs flake for reproducible builds and dev environments!";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.11";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
     systems.url = "github:nix-systems/default";
-    devenv = {
-      url = "github:cachix/devenv";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   nixConfig = {
@@ -18,97 +16,100 @@
   outputs = {
     self,
     nixpkgs,
-    systems,
     devenv,
+    systems,
     ...
   } @ inputs: let
     forEachSystem = nixpkgs.lib.genAttrs (import systems);
     postgresHost = "127.0.0.1";
     postgresPort = 5566;
   in {
-    packages = forEachSystem (
-      system: let
-        pkgs = import nixpkgs {inherit system;};
-      in {
-        # For setting up devenv
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
-
-        restartServices = pkgs.writeShellApplication {
-          name = "Sanitas dev server restarter";
-          runtimeInputs = with pkgs; [ansi];
-          text = ''
-            echo -e "$(ansi yellow)"WARNING:"$(ansi reset)" This script must be run on the project root directory!
-
-            echo "Trying to remove old .devenv..."
-            rm ./.devenv/state/postgres || rm -r ./.devenv/state/postgres || true
-
-            echo "Entering devshell..."
-            nix develop --impure . -c devenv up
-          '';
-        };
-
-        build-web-server = pkgs.writeShellApplication {
-          name = "NixOS Blogs WebServer build";
-          runtimeInputs = with pkgs; [
-            ansi
-          ];
-
-          text = ''
-                  echo -e "$(ansi yellow)Building frontend...$(ansi reset)"
-                  nix develop --extra-experimental-features flakes --extra-experimental-features nix-command --impure --command bash -c "cd ./nixos_blog_frontend/ && yarn build"
-            rm -r ./nixos_blog_frontend/node_modules || rm ./nixos_blog_frontend/node_modules || true
-
-                  echo -e "$(ansi yellow)Deleting static dir in backend if exists...$(ansi reset)"
-                  rm -r ./nixos_blog_backend/static/ || rm ./nixos_blog_backend/static || true
-
-                  echo -e "$(ansi yellow)Copying frontend build to backend...$(ansi reset)"
-                  cp -r ./nixos_blog_frontend/dist ./nixos_blog_backend/static
-          '';
-        };
-        run-web-server = pkgs.writeShellApplication {
-          name = "NixOS Blogs WebServer run";
-          runtimeInputs = with pkgs; [
-            ansi
-          ];
-
-          text = ''
-            echo -e "$(ansi yellow)Starting server... $(ansi reset)"
-                  nix develop --extra-experimental-features flakes --extra-experimental-features nix-command --impure --command bash -c "cd ./nixos_blog_backend/ && yarn start"
-          '';
-        };
-      }
-    );
-
-    devShells = forEachSystem (system: let
+    packages = forEachSystem (system: let
       pkgs = import nixpkgs {inherit system;};
-      strFromDBFile = file: builtins.readFile ./db/${file};
-      dbInitFile = builtins.concatStringsSep "\n" [(strFromDBFile "init.sql") (strFromDBFile "tables.sql")];
     in {
-      default = devenv.lib.mkShell {
-        inherit pkgs inputs;
-        modules = [
-          {
-            packages = with pkgs; [
-              dprint # Javascript formatter
-              oxlint # Javascript linter
-            ];
+      devenv-up = self.devShells.${system}.default.config.procfileScript;
 
-            languages.javascript = {
-              enable = true;
-              yarn = {
-                enable = true;
-              };
-            };
+      # Restart dev environment services...
+      restartServices = pkgs.writeShellApplication {
+        name = "Sanitas dev server restarter";
+        runtimeInputs = with pkgs; [ansi];
+        text = ''
+          echo -e "$(ansi yellow)"WARNING:"$(ansi reset)" This script must be run on the project root directory!
 
-            services.postgres = {
-              enable = true;
-              listen_addresses = postgresHost;
-              port = postgresPort;
-              initialScript = dbInitFile;
-            };
-          }
+          echo "Trying to remove old .devenv..."
+          rm ./.devenv/state/postgres || rm -r ./.devenv/state/postgres || true
+
+          echo "Entering devshell..."
+          nix develop --impure . -c devenv up
+        '';
+      };
+
+      # Build the web server for production...
+      build-web-server = pkgs.writeShellApplication {
+        name = "NixOS Blogs WebServer build";
+        runtimeInputs = with pkgs; [
+          ansi
         ];
+
+        text = ''
+          -e "$(ansi yellow)Building frontend...$(ansi reset)"
+          nix develop --extra-experimental-features flakes --extra-experimental-features nix-command --impure --command bash -c "cd ./nixos_blog_frontend/ && yarn build"
+          rm -r ./nixos_blog_frontend/node_modules || rm ./nixos_blog_frontend/node_modules || true
+
+          echo -e "$(ansi yellow)Deleting static dir in backend if exists...$(ansi reset)"
+          rm -r ./nixos_blog_backend/static/ || rm ./nixos_blog_backend/static || true
+
+          echo -e "$(ansi yellow)Copying frontend build to backend...$(ansi reset)"
+          cp -r ./nixos_blog_frontend/dist ./nixos_blog_backend/static
+        '';
+      };
+
+      # Run the web server in production...
+      run-web-server = pkgs.writeShellApplication {
+        name = "NixOS Blogs WebServer run";
+        runtimeInputs = with pkgs; [
+          ansi
+        ];
+
+        text = ''
+          echo -e "$(ansi yellow)Starting server... $(ansi reset)"
+          nix develop --extra-experimental-features flakes --extra-experimental-features nix-command --impure --command bash -c "cd ./nixos_blog_backend/ && yarn start"
+        '';
       };
     });
+
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = import nixpkgs {inherit system;};
+        strFromDBFile = file: builtins.readFile ./db/${file};
+        dbInitFile = builtins.concatStringsSep "\n" [(strFromDBFile "init.sql") (strFromDBFile "tables.sql")];
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              packages = with pkgs; [
+                dprint # Javascript formatter
+                oxlint # Javascript linter
+              ];
+
+              languages.javascript = {
+                enable = true;
+                yarn = {
+                  enable = true;
+                };
+              };
+
+              services.postgres = {
+                enable = true;
+                listen_addresses = postgresHost;
+                port = postgresPort;
+                initialScript = dbInitFile;
+              };
+            }
+          ];
+        };
+      });
   };
 }
