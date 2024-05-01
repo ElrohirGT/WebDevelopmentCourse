48134c8136bd46e0b39232ae2baae91de6aa8c43
ElrohirGT <elrohirgt@gmail.com>
2024-04-30 20:28:49 -0600

    feat: Configured CORS

--- NixOSBlogs/nixos_blog_backend/index.js
+++ NixOSBlogs/nixos_blog_backend/index.js
@@ -1,4 +1,5 @@
 import bodyParser from "body-parser";
+import cors from "cors";
 import express, { json, static as expressStatic } from "express";
 import API_ROUTER from "./src/routes/index.js";
 import { log, logger } from "./src/utils/log.js";
@@ -8,7 +9,12 @@ const host = "0.0.0.0";
 // Custom port from env variable
 // Usefull for railway deployment
 const port = process.env.PORT || 3000;
+const corsConfig = {
+  origin: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
 
+app.use(cors(corsConfig));
 app.use(logger);
 app.use(bodyParser.json({ limit: "15mb" }));
 app.use(expressStatic("static"));
