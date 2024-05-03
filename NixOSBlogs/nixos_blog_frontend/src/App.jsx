bc26905ecd10a9a7ba17196639a33fc6b560a77f
ElrohirGT <elrohirgt@gmail.com>
2024-05-02 20:03:54 -0600

    fix: Added basic routing

--- NixOSBlogs/nixos_blog_frontend/src/App.jsx
+++ NixOSBlogs/nixos_blog_frontend/src/App.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 import MainView from "src/views/MainView";
 
+export const ROUTES = {
+	home: "HOME",
+	admin: "ADMIN",
+	blogForm: "BLOGFORM",
+	blogDetails: "DETAILS",
+};
+
 export default function App() {
-	return <MainView />;
+	const [currentRoute, setCurrentRoute] = useState(ROUTES.home);
+	const navigateToLogin = () => {
+		setCurrentRoute(ROUTES.admin)
+	}
+
+	const routeToComponentMapper = {};
+	routeToComponentMapper[ROUTES.home] = <MainView navigateToLogin={navigateToLogin} />;
+
+	return routeToComponentMapper[currentRoute]
 }
--- NixOSBlogs/nixos_blog_frontend/src/views/Router.jsx
+++
@@ -1,6 +0,0 @@
-/**
-	* @param {Object} props 
-	* @param {string} props.currentRoute 
-	*/
-export default function Router({ currentRoute }) {
-}
