f846263b0e9e480efc2d209da0472f1107b4860b
ElrohirGT <elrohirgt@gmail.com>
2024-05-04 00:38:35 -0600

    fix: blogform styles

--- NixOSBlogs/nixos_blog_frontend/src/components/BlogForm/index.jsx
+++ NixOSBlogs/nixos_blog_frontend/src/components/BlogForm/index.jsx
@@ -20,91 +20,92 @@ import { logError } from "src/utils/general";
  * @param {BlogFormProps} props
  */
 export default function BlogForm({ blogInfo, onSubmit }) {
-  const [title, setTitle] = useState(blogInfo?.title ?? "");
-  const [content, setContent] = useState(blogInfo?.content ?? "");
-  const [banner, setBanner] = useState(blogInfo?.banner ?? "");
-  const [canSubmit, setCanSubmit] = useState(true);
+	const [title, setTitle] = useState(blogInfo?.title ?? "");
+	const [content, setContent] = useState(blogInfo?.content ?? "");
+	const [banner, setBanner] = useState(blogInfo?.banner ?? "");
+	const [canSubmit, setCanSubmit] = useState(true);
 
-  const onSubmitForm = () => {
-    if (!canSubmit) {
-      return;
-    }
+	const onSubmitForm = () => {
+		if (!canSubmit) {
+			return;
+		}
 
-    const id = blogInfo?.id ?? 0;
-    const formBlog = {
-      id,
-      title,
-      banner,
-      content,
-    };
-    onSubmit({ ...blogInfo, ...formBlog });
-  };
+		const id = blogInfo?.id ?? 0;
+		const formBlog = {
+			id,
+			title,
+			banner,
+			content,
+		};
+		onSubmit({ ...blogInfo, ...formBlog });
+	};
 
-  const contentPlaceholder = `
+	const contentPlaceholder = `
 Contenido:
 # Título del blog
 Blog de ejemplo con un [link](www.google.com)
 	`;
 
-  return (
-    <>
-      <input
-        type="text"
-        placeholder="Título:"
-        className="FormInput"
-        defaultValue={blogInfo?.title ?? ""}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <img
-        src={banner}
-        className="FormBanner"
-        style={{
-          height: banner ? "30vh" : "0px",
-          alignSelf: "center",
-          width: "auto",
-          maxWidth: "100%",
-        }}
-      />
-      <input
-        type="file"
-        placeholder="Banner:"
-        onChange={async (e) => {
-          setCanSubmit(false);
-          const file = e.target.files[0];
-          const dataURL = await new Promise((res, rej) => {
-            const reader = new FileReader();
-            reader.addEventListener("loadend", (_) => res(reader.result));
-            reader.addEventListener("error", rej);
-            reader.readAsDataURL(file);
-          }).catch(
-            logError("An error ocurred while retrieving banner binary data!"),
-          );
+	return (
+		<>
+			<input
+				type="text"
+				placeholder="Título:"
+				className="FormInput"
+				defaultValue={blogInfo?.title ?? ""}
+				onChange={(e) => setTitle(e.target.value)}
+			/>
+			<img
+				src={banner}
+				className="FormBanner"
+				style={{
+					height: banner ? "30vh" : "0px",
+					alignSelf: "center",
+					width: "auto",
+					maxWidth: "100%",
+				}}
+			/>
+			<input
+				type="file"
+				placeholder="Banner:"
+				onChange={async (e) => {
+					setCanSubmit(false);
+					const file = e.target.files[0];
+					const dataURL = await new Promise((res, rej) => {
+						const reader = new FileReader();
+						reader.addEventListener("loadend", (_) => res(reader.result));
+						reader.addEventListener("error", rej);
+						reader.readAsDataURL(file);
+					}).catch(
+						logError("An error ocurred while retrieving banner binary data!"),
+					);
 
-          if (dataURL === undefined) {
-            return;
-          }
+					if (dataURL === undefined) {
+						return;
+					}
 
-          setBanner(dataURL);
-          setCanSubmit(true);
-        }}
-      />
-      <textarea
-        style={{
-          height: "10rem",
-        }}
-        className="FormInput"
-        placeholder={contentPlaceholder}
-        defaultValue={blogInfo?.content ?? ""}
-        onChange={(e) => setContent(e.target.value)}
-      />
-      <button
-        onClick={onSubmitForm}
-        disabled={!canSubmit}
-        type="button"
-        className="PrimaryButton"
-      >
-        Subir
-      </button>
-    </>
-  );
+					setBanner(dataURL);
+					setCanSubmit(true);
+				}}
+			/>
+			<textarea
+				style={{
+					height: "10rem",
+					flexGrow: 1,
+				}}
+				className="FormInput"
+				placeholder={contentPlaceholder}
+				defaultValue={blogInfo?.content ?? ""}
+				onChange={(e) => setContent(e.target.value)}
+			/>
+			<button
+				onClick={onSubmitForm}
+				disabled={!canSubmit}
+				type="button"
+				className="PrimaryButton"
+			>
+				Subir
+			</button>
+		</>
+	);
 }
