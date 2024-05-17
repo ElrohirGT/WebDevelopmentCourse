e4613bf564a88d19edcf28334f174552e209d6c4
ElrohirGT <elrohirgt@gmail.com>
2024-05-16 19:50:20 -0600

    feat: initialized the project

---
+++ LabTesting/flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "¡Lab testing flake for reproducible builds and dev environments!";
+
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages = forEachSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devenv-up = self.devShells.${system}.default.config.procfileScript;
+    });
+
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              packages = with pkgs; [
+                dprint # Javascript formatter
+                oxlint # Javascript linter
+              ];
+
+              languages.javascript = {
+                enable = true;
+                yarn = {
+                  enable = true;
+                };
+              };
+            }
+          ];
+        };
+      });
+  };
+}
