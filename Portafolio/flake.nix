66961b96e95849ea4f4056324a35626587c10d4b
ElrohirGT <elrohirgt@gmail.com>
2024-05-18 19:16:34 -0600

    feat: initialized flake env

---
+++ Portafolio/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Flake for ElrohirGT's portfolio";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    systems,
+  }: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    devShells = forEachSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            elmPackages.elm
+          ];
+        };
+      }
+    );
+  };
+}
